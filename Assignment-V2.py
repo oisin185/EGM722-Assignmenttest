52544fbf9720ed77b052a8c1e5c056c26eac35dd
oisin185 <165175309+oisin185@users.noreply.github.com>
2024-05-08 00:01:06 +0100

    2nd Commit
    - Displayed the script for first time using "plt.show()",
    - "ShapelyFeature" used to import boundary shapefile,
              - changed from LGD to DEA (more accurate)
    - Added the axes and displayed
    - Set distance from the DEA shapefile the map
       boundary should be.
    - Corrected the "Environment" file and included it
       correctly.
              - Will add to this document as needed.

--- Assignment-V2.py
+++ Assignment-V2.py
@@ -1,22 +1,37 @@
-# import all modules needed for the script
+# Importing all modules needed for the script
 import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from PIL._imaging import outline
 from cartopy import crs as ccrs
+from cartopy.feature import ShapelyFeature
 import cartopy.feature as cfeature
 from matplotlib_scalebar.scalebar import ScaleBar
 
-# Load shapefiles needed for the scirpt
-belfast_lgd_shapefile = 'C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastLGD/Belfast_LGD.shp'
+# Load shapefiles needed for the script
+belfast_DEA_shapefile = 'C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastDEA/Belfast_DEA.shp' # Replaced the LGD boundary with the smaller DEA boundary for greater detail
 belfast_busstops_shapefile = 'C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastBusstop/Belfast_Busstops.shp'
 belfast_roads_shapefile = 'C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastRoads/Belfast_Roads.shp'
+belfast_rail_shapefile = 'C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastRail/Railplatforms.shp' # Added the railway stops to the script
+# Load the Belfast DEA outline, bus stop locations, railway stops and road network into GeoDataFrames which contains all the
+# attributes related to the datasets
 
-# Load the Belfast LGD outline, busstop locations and road network into GeoDataFrames which contains all the attributes related to the datasets
-lgd = gpd.read_file(os.path.abspath('C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastLGD/Belfast_LGD.shp'))
+dea = gpd.read_file(os.path.abspath('C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastDEA/Belfast_DEA.shp'))
 belfast_busstops = gpd.read_file(os.path.abspath('C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastBusstop/Belfast_Busstops.shp'))
 belfast_roads = gpd.read_file(os.path.abspath('C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastRoads/Belfast_Roads.shp'))
+belfast_rail = gpd.read_file(os.path.abspath('C:/Users/oisin/Documents/GitHub/EGM722-Assignmenttest/BelfastRail/Railplatforms.shp'))
 
-belfast_utm = ccrs.UTM(30) # setting the correct the Coordinate Reference System (CRS) for the map
+belfast_utm = ccrs.UTM(30) # Setting the correct the Coordinate Reference System (CRS) for the map
 
-fig = plt.figure(figsize=(10, 10)) # The boundary of the map which will be created and setting the size of 10 inches by 10 inches
+fig = plt.figure(figsize=(15, 15)) # The boundary of the map which will be created and setting the size of 15 by 15 inches
 axes = plt.axes(projection=belfast_utm) # Projecting the axes of the boundary into the correct CRS
+
+# ShapelyFeature to use 'dea' geometry attribute, specifying the CRS and colours for the outline and background
+belfast_dea_outline = ShapelyFeature(dea['geometry'], belfast_utm, edgecolor='k', facecolor='w')
+axes.add_feature(belfast_dea_outline) # Adding the map boundary line
+
+xmin, ymin, xmax, ymax = dea.total_bounds # Displaying the boundaries of the Belfast LGD shapefile
+axes.set_extent([xmin-500, xmax+500, ymin-500, ymax+500], crs=belfast_utm) # Scaling the map to be 500m from the map boundary and setting the CRS
+
+
+plt.show() # Used this command throughout the editing phase of creating the script to ensure I could see if the data was loading correctly
